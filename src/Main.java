0b4a0eee2bdf9f4e396784cbc904a57f2fb49480
khanhdzx <khanhvannguyen2303@gmail.com>
2022-09-30 09:54:05 +0700

    ja

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Student.change();
+
+        Student s1 = new Student(111,"chinh");
+        Student s2 = new Student(222,"toan");
+        Student s3 = new Student(333,"dung");
+        s1.díplay();
+        s2.díplay();
+        s3.díplay();
+    }
+}
